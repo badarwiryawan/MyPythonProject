a6350652693306605065573d6b0f918d7336eb10
badarwiryawan <145012387+badarwiryawan@users.noreply.github.com>
2023-10-27 17:56:22 +0700

    Update Kalkulator Valuasi Pendekatan Discounted Cash Flow.py
    This update is added support for incremental income.

--- Kalkulator Valuasi Pendekatan Discounted Cash Flow.py
+++ Kalkulator Valuasi Pendekatan Discounted Cash Flow.py
@@ -7,15 +7,22 @@ import locale
 
 locale.setlocale(locale.LC_ALL, "id_ID")
 Net_Income = float(input("Masukkan Pendapatan: "))
+Increment = float(input("Masukkan Nominal Kenaikan Pendapatan: "))
 Discount_Rate = float(input("Masukkan Tingkat Diskonto: "))
 Periods = int(input("Masukkan Lamanya Tahun: "))
 
 #-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
 
-def pendekatan_discounted_cash_flow(Pendapatan=Net_Income, Tingkat_Diskonto=Discount_Rate, Periode=Periods):
+def pendekatan_discounted_cash_flow(Pendapatan=Net_Income, Inkremental=Increment, Tingkat_Diskonto=Discount_Rate, Periode=Periods):
+    Daftar_Pendapatan = []
+
+    for n in range(Periode):
+        Daftar_Pendapatan.append(Pendapatan)
+        Pendapatan += Inkremental
+        
     Nilai = 0
-    for n in range(1, Periode + 1):
-        Nilai += Pendapatan *  (1 / ((1 + Tingkat_Diskonto) ** n))
+    for n, o in zip(range(1, Periode + 1), range(0, Periode)):
+        Nilai += Daftar_Pendapatan[o] *  (1 / ((1 + Tingkat_Diskonto) ** n))
     return Nilai
 
 #-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
