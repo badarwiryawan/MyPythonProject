e74fa678c7e7fea519e3b722761a00d2c0907b09
badarwiryawan <145012387+badarwiryawan@users.noreply.github.com>
2023-11-07 21:47:55 +0700

    Update Kalkulator Valuasi Pendekatan Discounted Cash Flow.py

--- Kalkulator Valuasi Pendekatan Discounted Cash Flow.py
+++ Kalkulator Valuasi Pendekatan Discounted Cash Flow.py
@@ -1,42 +1,78 @@
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
+#Program ini memerlukan library Pandas, install dengan command "pip install pandas"
 
 
-import locale
 
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
 
-locale.setlocale(locale.LC_ALL, "id_ID")
+#-------------------------------KALKULATOR VALUASI PENDEKATAN PENDAPATAN OLEH BADAR WIRYAWAN-----------------------------------------
+
+import pandas as pd
+
 Net_Income = float(input("Masukkan Pendapatan: "))
 Increment = float(input("Masukkan Nominal Kenaikan Pendapatan: "))
 Discount_Rate = float(input("Masukkan Tingkat Diskonto: "))
 Periods = int(input("Masukkan Lamanya Tahun: "))
 
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
+#-------------------------------KALKULATOR VALUASI PENDEKATAN PENDAPATAN OLEH BADAR WIRYAWAN-----------------------------------------
 
 def pendekatan_discounted_cash_flow(Pendapatan=Net_Income, Inkremental=Increment, Tingkat_Diskonto=Discount_Rate, Periode=Periods):
-    
+
+    #PENAMBANGAN DATA DAFTAR PENDAPATAN
+
     Daftar_Pendapatan = []
 
-    for n in range(Periode):
+    for b in range(Periode):
         Daftar_Pendapatan.append(Pendapatan)
         Pendapatan += Inkremental
+
+    #PENAMBANGAN DATA DAFTAR PV FACTOR
+
+    Daftar_PV_Factor = []
+
+    for b in range(1, Periode+1):
+        PV_Factor = 1 / ((1 + Tingkat_Diskonto) ** b)
+        Daftar_PV_Factor.append(PV_Factor)
+
+    #PENGHITUNGAN PENDAPATAN DISESUAIKAN (PRESENT VALUE)
+
+    Daftar_Pendapatan_Adjusted = []
+
+    for c, d in zip(range(0, Periode), range(0, Periode)):
+        Pendapatan_Adj = round(Daftar_Pendapatan[c] * Daftar_PV_Factor[d], 4)
+        Daftar_Pendapatan_Adjusted.append(Pendapatan_Adj)
+
+    #PENGHITUNGAN PENDAPATAN KUMULATIF
+    #Ini adalah nilai valuasi tahun ke-n
+
+    Daftar_Pendapatan_Kumulatif = []
+
+    Revenue = 0
+    for e in Daftar_Pendapatan_Adjusted:
+        Revenue += e
+        Daftar_Pendapatan_Kumulatif.append(Revenue)
         
-    Nilai = 0
-    for n, o in zip(range(1, Periode + 1), range(0, Periode)):
-        Nilai += Daftar_Pendapatan[o] *  (1 / ((1 + Tingkat_Diskonto) ** n))
-    return Nilai
+    #PENGINPUTAN DATA KE DALAM TABEL
 
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
+    tables = {"Tahun": range(1, Periode+1), "Pendapatan": Daftar_Pendapatan, "PV Factor": Daftar_PV_Factor, "Pendapatan Disesuaikan": Daftar_Pendapatan_Adjusted, "Pendapatan Kumulatif": Daftar_Pendapatan_Kumulatif}
+    data = pd.DataFrame(tables)
 
-Nominal_dalam_rupiah = locale.currency(pendekatan_discounted_cash_flow(), grouping=True)
+    #EXPORT KE FILE CSV
 
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
+    Cetak = input("Ingin mencetak hasil (Y/N)?: ")
 
-print(Nominal_dalam_rupiah)
+    if Cetak == "Y":
+        data.to_csv("Kalkulasi_Pendekatan_Pendapatan.csv")
+    elif Cetak == "N":
+        print(data)
 
-#-------------------KALKULATOR VALUASI PENDEKATAN DISCOUNTED CASH FLOW OLEH BADAR WIRYAWAN------------------
+        Konfirmasi_Cetak = input("Apakah Anda ingin mencetaknya kali ini (Y/N)?: ")
 
-     
-    
-    
-    
+        if Konfirmasi_Cetak == "Y":
+            data.to_csv("Kalkulasi_Pendekatan_Pendapatan.csv")
+        elif Konfirmasi_Cetak == "N":
+            print("Baiklah :)")
+ 
+#-------------------------------KALKULATOR VALUASI PENDEKATAN PENDAPATAN OLEH BADAR WIRYAWAN-----------------------------------------
+
+print(pendekatan_discounted_cash_flow())
+
+#-------------------------------KALKULATOR VALUASI PENDEKATAN PENDAPATAN OLEH BADAR WIRYAWAN-----------------------------------------
