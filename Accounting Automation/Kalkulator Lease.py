e5ae3bcd0da52fb699e63ad588e0111d4b5316c1
badarwiryawan <145012387+badarwiryawan@users.noreply.github.com>
2023-12-19 17:27:44 +0700

    Update Kalkulator Lease.py
    Adding transaction journal feature.

--- Accounting Automation/Kalkulator Lease.py
+++ Accounting Automation/Kalkulator Lease.py
@@ -6,12 +6,17 @@
 #-----------------------------------KALKULATOR LEASE OLEH BADAR WIRYAWAN---------------------------------------------
 
 import pandas as pd
+import locale
+import datetime
+from datetime import datetime
 
 #-----------------------------------KALKULATOR LEASE OLEH BADAR WIRYAWAN---------------------------------------------
 
 Pembayaran_sewa = float(input("Masukkan pembayaran sewa (tahun): "))
 Bunga_sewa = float(input("Masukkan tingkat bunga sewa (tahun): "))
 Periode = int(input("Masukkan lamanya sewa (tahun): "))
+
+locale.setlocale(locale.LC_ALL, "id_ID")
 
 #-----------------------------------KALKULATOR LEASE OLEH BADAR WIRYAWAN---------------------------------------------
 
@@ -64,9 +69,7 @@ def lease_schedule(Lease_payment = Pembayaran_sewa, Lease_rate = Bunga_sewa, Per
 
     for e in range(Periods):
         hak_guna_aset_baru = daftar_hak_guna_aset[e] - daftar_beban_amortisasi[e + 1]
-        daftar_hak_guna_aset.append(hak_guna_aset_baru)
-    
-    
+        daftar_hak_guna_aset.append(hak_guna_aset_baru)  
 
     for f in range(Periods):
         akumulasi_amortisasi = daftar_akumulasi_amortisasi[f] + daftar_beban_amortisasi[f + 1]
@@ -83,17 +86,50 @@ def lease_schedule(Lease_payment = Pembayaran_sewa, Lease_rate = Bunga_sewa, Per
               }
     data = pd.DataFrame(tables)
 
-    Cetak_hasil = input("Apakah Anda ingin mencetak hasil (Y/N)?: ")
+    Cetak_hasil = input("Apakah Anda ingin mencetak hasilnya (Y/N)?: ")
+    today_date = datetime.now().date()
+
     if Cetak_hasil == "Y":
-        data.to_csv("Kalkulasi_Lease.csv")
+        data.to_csv(f"Kalkulasi_Lease_{str(today_date)}.csv")
     elif Cetak_hasil == "N":
         print(data)
 
-        Konfirmasi_cetak = input("Apakah Anda ingin mencetaknya kali ini (Y/N)?: ")
-        if Konfirmasi_cetak == "Y":
-            data.to_csv("Kalkulasi_Lease.csv")
-        elif Konfirmasi_cetak == "N":
+        Jurnal_transaksi = input("Apakah Anda ingin melihat jurnal transaksinya (Y/N)?: ")
+        if Jurnal_transaksi == "Y":
+            rupiah_hak_guna_aset = locale.currency(daftar_hak_guna_aset[0], grouping=True)
+            rupiah_utang_sewa = locale.currency(daftar_utang_sewa[0], grouping=True)
+
+            daftar_rupiah_pengurangan_utang_sewa = []
+            daftar_rupiah_beban_bunga = []
+            daftar_rupiah_beban_amortisasi = []
+            daftar_rupiah_pembayaran = []
+
+            for g in range(0, Periods + 1):
+                rupiah_pengurangan_utang_sewa = locale.currency(daftar_pengurangan_utang_sewa[g], grouping=True)
+                rupiah_beban_bunga = locale.currency(daftar_beban_bunga[g], grouping=True)
+                rupiah_beban_amortisasi = locale.currency(daftar_beban_amortisasi[g], grouping=True)
+                rupiah_pembayaran = locale.currency(daftar_pembayaran[g], grouping=True)
+                daftar_rupiah_pengurangan_utang_sewa.append(rupiah_pengurangan_utang_sewa)
+                daftar_rupiah_beban_bunga.append(rupiah_beban_bunga)
+                daftar_rupiah_beban_amortisasi.append(rupiah_beban_amortisasi)
+                daftar_rupiah_pembayaran.append(rupiah_pembayaran)
+
+            print("Hak Guna Aset" + " " * 26 + f"{rupiah_hak_guna_aset}")
+            print(" " * 4 + "Utang Sewa" + " " * 32 + f"{rupiah_utang_sewa}")
+            print("(" + "\033[3m" + "Pencatatan Hak Sewa" + "\033[0m" + ")")
+            print(" ")
+            for h in range(1, Periods + 1):
+                print("Utang Sewa" + " " * 29 + f"{daftar_rupiah_pengurangan_utang_sewa[h]}" )
+                print("Beban Bunga" + " " * 28 + f"{daftar_rupiah_beban_bunga[h]}")
+                print("Beban Amortisasi Hak Guna Aset" + " " * 9 + f"{daftar_rupiah_beban_amortisasi[h]}")
+                print(" " * 4 + "Akumulasi Amortisasi Hak Guna Aset" + " " * 8 + f"{daftar_rupiah_beban_amortisasi[h]}")
+                print(" " * 4 + "Kas" + " " * 39 + f"{daftar_rupiah_pembayaran[h]}" )
+                print("(" + "\033[3m" + f"Pembayaran Pokok, Bunga, dan Pencatatan Amortisasi ke-{h}" + "\033[0m" + ")")
+                print(" ")
+        
+        elif Jurnal_transaksi == "N":
             print("Baiklah :)")
+
 
 #-----------------------------------KALKULATOR LEASE OLEH BADAR WIRYAWAN---------------------------------------------
 
