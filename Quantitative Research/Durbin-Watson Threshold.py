84510944bb8d275e72452639f11ca489dd45eb6f
badarwiryawan <145012387+badarwiryawan@users.noreply.github.com>
2024-11-03 14:11:43 +0700

    Create Durbin-Watson Threshold.py
    Program untuk mencari threshold Durbin-Watson secara otomatis.

---
+++ Quantitative Research/Durbin-Watson Threshold.py
@@ -0,0 +1,39 @@
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
+
+import pandas as pd
+
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
+
+#JUMLAH MAKSIMUM OBSERVASI = 2000
+#JUMLAH MAKSIMUM VARIABEL INDEPENDEN = 21
+#UNTUK OBSERVASI LEBIH DARI 200, BULATKAN KE KELIPATAN 10 TERDEKAT. MISAL 223 --> 220, 277 --> 280
+
+Observasi = int(input("Jumlah Observasi = "))
+Variabel_Independen = int(input("Jumlah Variabel Independen = "))
+
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
+
+data = pd.read_csv("https://docs.google.com/spreadsheets/d/1PJQrU4Z5fn9sc5aubZPKvVwkeY7sy3o2/export?format=csv")
+
+#SUMBER: https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://wernermurhadi.files.wordpress.com/2011/07/tabel-durbin-watson.pdf&ved=2ahUKEwjKttTXzL-JAxXFoGMGHQogGfQQFnoECCIQAQ&usg=AOvVaw0a_SHb1RD8hAK-MTEj9u2v
+
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
+
+locate = data[(data["T"] == Observasi) & (data["K"] == Variabel_Independen + 1)].index
+locate.to_list()
+
+dL = data["dL"].iloc[locate[0]]
+dU = data["dU"].iloc[locate[0]]
+
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
+
+print("-----------------------------------------------")
+print("THRESHOLD DURBIN WATSON")
+print(f"Observasi = {Observasi}, Variabel Independen = {Variabel_Independen}")
+print(" ")
+print(f"Batas bawah (dL) = {dL}")
+print(f"Batas atas (dU) = {dU}")
+print(f"4-dU = {4-dU}")
+print("-----------------------------------------------")
+
+#------------------------------------DURBIN WATSON THRESHOLD BY BADAR WIRYAWAN-------------------------------------
